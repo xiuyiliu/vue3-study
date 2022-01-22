adf5f00562a7855b339069f82c75e43ddd279bf7
xiuyiliu <1103058876@qq.com>
2022-01-22 17:13:48 +0800

    eslint配置

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'vue/multi-word-component-names': 0
+  }
+}
